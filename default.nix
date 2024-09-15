098f8e6ef949268ccbd94359224ed0ff40dd0e04
Steffen Vogel <post@steffenvogel.de>
2024-09-15 18:17:18 +0200

    nix: Update vendorHash
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ buildGoModule {
   pname = "pductl";
   version = "0.1.0";
   src = ./.;
-  vendorHash = "sha256-HoiYuyzHctwuwfEdySeMjdFe4juyqj+dPZ5U7OADdSc=";
+  vendorHash = "sha256-Td9f39DAvl440K09yddMjnskxmjoCc9hYgxFASGG5hE=";
 
   meta = {
     mainProgram = "pdud";
